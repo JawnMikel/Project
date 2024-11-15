d83f4cac2721d2fc3118ad73ee6bce7dda88901d
JawnMikel <149331743+JawnMikel@users.noreply.github.com>
2024-11-14 22:31:45 -0500

    Made Director class, made media classes, Actor class
    Made the Director, media and actor classes.
    I populated all of them.
    In Media class I initialized it the functions, but I did not do anything to it yet.

---
+++ MovieDatabase/MovieDatabase/Director.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase
+{
+    public class Director
+    {
+        public int DirectorId { get; set; }
+        public List<Media> Directed {  get; set; }
+
+        public Director(int directorId, List<Media> directed)
+        {
+            DirectorId = directorId;
+            Directed = directed;
+        }
+    }
+}
