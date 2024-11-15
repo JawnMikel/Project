6e456723537ffbecbf29f75e13c1d782e66db5be
Danat <danatmuradov500@gmail.com>
2024-11-14 22:03:01 -0500

    Created two custom exceptions to handle database operation errors.

---
+++ MovieDatabase/MovieDatabase/Exceptions/DatabaseInitializationException.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase.Exceptions
+{
+    /// <summary>
+    /// DatabaseInitializationException is used to represent exceptions thrown
+    /// when attempting to initialize (create) the database.
+    /// </summary>
+    public class DatabaseInitializationException : DatabaseException
+    {
+        public DatabaseInitializationException(string? message) : base(message)
+        {
+        }
+    }
+}
