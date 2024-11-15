a587bf2aee387027d7ef221d5bbedf566dee5e18
JawnMikel <149331743+JawnMikel@users.noreply.github.com>
2024-11-14 22:52:51 -0500

    Made movie class and updated the episode class
    made movie class and updated episode class.
    
    added a episodeId field

--- MovieDatabase/MovieDatabase/Episode.cs
+++ MovieDatabase/MovieDatabase/Episode.cs
@@ -8,12 +8,14 @@ namespace MovieDatabase
 {
     public class Episode
     {
+        public int EpisodeId { get; set; }
         public int Duration { get; set; }
         public int SeasonNumber { get; set; }
         public int EpisodeNumber { get; set; }
 
-        public Episode(int duration, int seasonNumber, int episodeNumber)
+        public Episode(int episodeId, int duration, int seasonNumber, int episodeNumber)
         {
+            EpisodeId = episodeId;
             Duration = duration;
             SeasonNumber = seasonNumber;
             EpisodeNumber = episodeNumber;
