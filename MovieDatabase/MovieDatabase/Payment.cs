96990b2b088361d1d61e0a91b48ea3e79ab4b4c7
JawnMikel <149331743+JawnMikel@users.noreply.github.com>
2024-11-14 23:07:10 -0500

    Create Payment.cs
    populated the class. Made method but did not do anything to it yet.
    not sure about the CardExpirationDate so i added it as a comment for now

---
+++ MovieDatabase/MovieDatabase/Payment.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieDatabase
+{
+    public class Payment
+    {
+        public string CardHolderName { get; set; }
+        public string CreditCardNum { get; set; }
+        //public DateTime CardExpirationDate { get; set; }
+        public int CVV {  get; set; }
+
+        public Payment(string cardHolderName, string creditCardNum, int cVV)
+        {
+            CardHolderName = cardHolderName;
+            CreditCardNum = creditCardNum;
+            CVV = cVV;
+        }
+
+        public bool validateCard(string creditCardNumber)
+        {
+            return false;
+        }
+    }
+}
