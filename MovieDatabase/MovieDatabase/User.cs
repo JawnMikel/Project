bb3c649381e0b6a0560b5a7d4a292b415c1b12e2
Danat <danatmuradov500@gmail.com>
2024-11-15 23:20:12 -0500

    Changed access modifiers of the username and password fields, and made them properties to allow easier insertion operation to the database.

--- MovieDatabase/MovieDatabase/User.cs
+++ MovieDatabase/MovieDatabase/User.cs
@@ -11,9 +11,9 @@ namespace MovieDatabase
         private static int nextId = 1;
         public int Id { get; set; }
         
-        private string _username;
+        public string UserName { get; set; }
         
-        private string _password;
+        public string Password { get; set; }
         public string FirstName { get; set; }
         public string LastName { get; set; }
         // public Date Dob { get; set; }
@@ -27,8 +27,8 @@ namespace MovieDatabase
         public User(string username, string password, string firstName, string lastName, Memberships membership)
         {
             Id = nextId++;
-            _username = username;
-            _password = password;
+            UserName = username;
+            Password = password;
             FirstName = firstName;
             LastName = lastName;
             Membership = membership;
