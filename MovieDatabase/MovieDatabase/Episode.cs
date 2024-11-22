9403a921431b0ad82017d41c5d768b7a7df41f82
Danat <danatmuradov500@gmail.com>
2024-11-21 22:24:08 -0500

    Updated the Episode class for database insertion. Made the class extend Media.

--- MovieDatabase/MovieDatabase/Episode.cs
+++ MovieDatabase/MovieDatabase/Episode.cs
@@ -6,14 +6,14 @@ using System.Threading.Tasks;
 
 namespace MovieDatabase
 {
-    public class Episode
+    public class Episode : Media
     {
         public int EpisodeId { get; set; }
         public int Duration { get; set; }
         public int SeasonNumber { get; set; }
         public int EpisodeNumber { get; set; }
 
-        public Episode(int episodeId, int duration, int seasonNumber, int episodeNumber)
+        public Episode(int mediaId, string title, DateTime releaseDate, List<Director> directors, List<Actor> actors, Genre genres, List<Review> reviews, string sysnopsis, int episodeId, int duration, int seasonNumber, int episodeNumber) : base(mediaId, title, releaseDate, directors, actors, genres, reviews, sysnopsis)
         {
             EpisodeId = episodeId;
             Duration = duration;
