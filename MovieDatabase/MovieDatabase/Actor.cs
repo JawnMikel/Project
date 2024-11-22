4e4b14537b28f7452c7a126c5bd77515c0850ec2
Danat <danatmuradov500@gmail.com>
2024-11-22 11:10:10 -0500

    Updated the Actor to extend to CrewMember class.

--- MovieDatabase/MovieDatabase/Actor.cs
+++ MovieDatabase/MovieDatabase/Actor.cs
@@ -6,12 +6,12 @@ using System.Threading.Tasks;
 
 namespace MovieDatabase
 {
-    public class Actor
+    public class Actor : CrewMember
     {
         public int ActorId { get; set; }
         public List<Media> Media {  get; set; }
 
-        public Actor(int actorId, List<Media> media)
+        public Actor(string firstName, string secondName, List<Review> reviews, int actorId, List<Media> media) : base(firstName, secondName, reviews)
         {
             ActorId = actorId;
             Media = media;
