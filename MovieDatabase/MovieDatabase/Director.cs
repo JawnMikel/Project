686b9ebd5029f395df5a2f3dc97dd0302735189c
Danat <danatmuradov500@gmail.com>
2024-11-28 20:05:23 -0500

    (Task #56) Refactored the Director class to avoid circular references.

--- MovieDatabase/MovieDatabase/Director.cs
+++ MovieDatabase/MovieDatabase/Director.cs
@@ -11,7 +11,9 @@ namespace MovieDatabase
     /// </summary>
     public class Director : CrewMember
     {
-        public List<Media> Directed { get; set; }
+        public List<int> DirectedMovies { get; }
+        public List<int> DirectedTVShows { get; }
+        public List<int> DirectedEpisodes { get; }
 
         /// <summary>
         /// All argument constructor for a new director.
@@ -21,11 +23,13 @@ namespace MovieDatabase
         /// <param name="imageLink">The link of the picture of the director.</param>
         public Director(string firstName, string secondName, string imageLink) : base(firstName, secondName, imageLink)
         {
-            Directed = new List<Media>();
+            DirectedMovies = new List<int>();
+            DirectedTVShows = new List<int>();
+            DirectedEpisodes = new List<int>();
         }
 
         /// <summary>
-        /// Add a media to the directed list of this director.
+        /// Add a media to the appropriate directed list of this director.
         /// </summary>
         /// <param name="media">The media to add.</param>
         /// <exception cref="ArgumentNullException">Exception thrown when the media argument is null.</exception>
@@ -35,11 +39,22 @@ namespace MovieDatabase
             {
                 throw new ArgumentNullException("The media argument cannot be null.");
             }
-            Directed.Add(media);
+            if (media is Movie)
+            {
+                DirectedMovies.Add(media.MediaId);
+            }
+            else if (media is TVShow)
+            {
+                DirectedTVShows.Add(media.MediaId);
+            }
+            else
+            {
+                DirectedEpisodes.Add(media.MediaId);
+            }
         }
 
         /// <summary>
-        /// Remove a media from the directed list of this director.
+        /// Remove a media from the appropriate directed list of this director.
         /// </summary>
         /// <param name="media">The media to remove.</param>
         /// <exception cref="ArgumentNullException">Exception thrown when the media argument is null.</exception>
@@ -49,7 +64,18 @@ namespace MovieDatabase
             {
                 throw new ArgumentNullException("The media argument cannot be null.");
             }
-            Directed.Remove(media);
+            if (media is Movie)
+            {
+                DirectedEpisodes.Remove(media.MediaId);
+            }
+            else if (media is TVShow)
+            {
+                DirectedTVShows.Remove(media.MediaId);
+            }
+            else
+            {
+                DirectedEpisodes.Remove(media.MediaId);
+            }
         }
 
         /// <summary>
