60ab00d9cb06b811ddbb0ee5186a96583b30058a
Danat <danatmuradov500@gmail.com>
2024-11-29 21:49:11 -0500

    Created sql file for storing data to be inserted.

---
+++ MovieDatabase/MovieDatabase/sql/DataInsertion.sql
@@ -0,0 +1,20 @@
+-- Insert statements for all possible genres
+INSERT INTO genre (GenreName) 
+VALUES 
+    ('ACTION'),
+    ('COMEDY'),
+    ('MYSTERY'),
+    ('HORROR'),
+    ('SCI_FI'),
+    ('ROMANCE'),
+    ('THRILLER'),
+    ('DRAMA'),
+    ('MUSICAL'),
+    ('WAR'),
+    ('SUPERHERO'),
+    ('ANIMATION'),
+    ('DOCUMENTARY'),
+    ('CRIME'),
+    ('FANTASY');
+
+-- Insert statements for 
